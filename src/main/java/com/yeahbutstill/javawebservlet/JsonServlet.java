3d7a867758174589886f5440e220ad0809cbf03e
yeahbutstill <mayadani28@gmail.com>
2023-04-18 22:42:04 +0700

    response

---
+++ src/main/java/com/yeahbutstill/javawebservlet/JsonServlet.java
@@ -0,0 +1,20 @@
+package com.yeahbutstill.javawebservlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/json")
+public class JsonServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String json = "{\"name\":\"John\", \"surname\":\"Doe\"}";
+        resp.setHeader("Content-Type", "application/json");
+        resp.getWriter().println(json);
+    }
+}

4b9e9b4ed7e2e1b94c408dccb44dc9fbe612dcd8
yeahbutstill <mayadani28@gmail.com>
2023-04-19 20:32:40 +0700

    request body

---
+++ src/main/java/com/yeahbutstill/javawebservlet/model/SayHelloRequest.java
@@ -0,0 +1,11 @@
+package com.yeahbutstill.javawebservlet.model;
+
+import lombok.Data;
+
+@Data
+public class SayHelloRequest {
+
+    private String username;
+    private String password;
+
+}
---
+++ src/main/java/com/yeahbutstill/javawebservlet/utils/JsonUtil.java
@@ -0,0 +1,13 @@
+package com.yeahbutstill.javawebservlet.utils;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtil {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    public static ObjectMapper getObjectMapper() {
+        return OBJECT_MAPPER;
+    }
+
+}

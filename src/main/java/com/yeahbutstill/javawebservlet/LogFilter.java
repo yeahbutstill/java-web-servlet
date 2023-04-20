b367564a91a504f64a07e6d340f3d2d8cd504606
yeahbutstill <mayadani28@gmail.com>
2023-04-21 04:15:39 +0700

    filter

---
+++ src/main/java/com/yeahbutstill/javawebservlet/LogFilter.java
@@ -0,0 +1,25 @@
+package com.yeahbutstill.javawebservlet;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class LogFilter extends HttpFilter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        LOGGER.info("Received request {}", request.getRequestURI());
+        chain.doFilter(request, response);
+    }
+
+}
